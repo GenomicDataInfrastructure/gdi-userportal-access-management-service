c2e93a161003b57b073af14fbc46614f3d9c867f
Bruno Pacheco <brunopacheco1@yahoo.com>
2024-03-29 00:40:04 +0100

    fix: review rems openapi doc

---
+++ src/main/java/io/github/genomicdatainfrastructure/daam/api/GlobalExceptionMapper.java
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package io.github.genomicdatainfrastructure.daam.api;
+
+import static jakarta.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+
+import io.github.genomicdatainfrastructure.daam.model.ErrorResponse;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+
+    @Override
+    public Response toResponse(Exception exception) {
+        var errorResponse = new ErrorResponse(
+                "Not expected exception",
+                INTERNAL_SERVER_ERROR.getStatusCode(),
+                exception.getMessage()
+        );
+
+        return Response
+                .status(INTERNAL_SERVER_ERROR)
+                .entity(errorResponse)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}

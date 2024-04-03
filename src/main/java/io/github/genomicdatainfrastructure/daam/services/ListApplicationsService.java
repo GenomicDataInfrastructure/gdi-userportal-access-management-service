7848746dcce0566eabfaacad8cd3d711d3685be9
Inderpal Singh <inderpal.singh@lnds.lu>
2024-04-03 14:30:11 +0200

    feat: Return datasets objects

--- src/main/java/io/github/genomicdatainfrastructure/daam/services/ListApplicationsService.java
+++ src/main/java/io/github/genomicdatainfrastructure/daam/services/ListApplicationsService.java
@@ -5,6 +5,7 @@
 package io.github.genomicdatainfrastructure.daam.services;
 
 import io.github.genomicdatainfrastructure.daam.gateways.RemsApiQueryGateway;
+import io.github.genomicdatainfrastructure.daam.gateways.RemsApplicationMapper;
 import io.github.genomicdatainfrastructure.daam.model.ListedApplication;
 import io.github.genomicdatainfrastructure.daam.remote.rems.model.ApplicationOverview;
 import io.github.genomicdatainfrastructure.daam.remote.rems.model.V2Resource;
@@ -15,35 +16,18 @@ import java.util.List;
 @ApplicationScoped
 public class ListApplicationsService {
 
+    private RemsApplicationMapper applicationMapper;
     private final RemsApiQueryGateway gateway;
 
     public ListApplicationsService(
+            RemsApplicationMapper applicationMapper,
             RemsApiQueryGateway remsApiQueryGateway
     ) {
+        this.applicationMapper = applicationMapper;
         this.gateway = remsApiQueryGateway;
     }
 
     public List<ListedApplication> listApplications(String userId) {
-        return gateway.listApplications(userId)
-                .stream()
-                .map(this::parse)
-                .toList();
-    }
-
-    public ListedApplication parse(ApplicationOverview applicationOverview) {
-        return ListedApplication.builder()
-                .id(applicationOverview.getApplicationId())
-                .title(applicationOverview.getApplicationExternalId())
-                .currentState(applicationOverview.getApplicationState().value())
-                .stateChangedAt(applicationOverview.getApplicationLastActivity())
-                .datasetIds(getDatasetIds(applicationOverview))
-                .build();
-    }
-
-    private List<String> getDatasetIds(ApplicationOverview applicationOverview) {
-        return applicationOverview.getApplicationResources()
-                .stream()
-                .map(V2Resource::getResourceExtId)
-                .toList();
+        return applicationMapper.from(gateway.listApplications(userId));
     }
 }

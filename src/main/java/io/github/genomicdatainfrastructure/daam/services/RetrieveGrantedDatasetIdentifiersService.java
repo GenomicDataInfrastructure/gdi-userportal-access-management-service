7bc28bf5ba158d8392820686ee24007859a12476
Emiliana <emiliana.pali@lnds.lu>
2024-05-30 16:20:48 +0200

    fix: #58 license added

--- src/main/java/io/github/genomicdatainfrastructure/daam/services/RetrieveGrantedDatasetIdentifiersService.java
+++ src/main/java/io/github/genomicdatainfrastructure/daam/services/RetrieveGrantedDatasetIdentifiersService.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package io.github.genomicdatainfrastructure.daam.services;
 
 import io.github.genomicdatainfrastructure.daam.gateways.RemsApiQueryGateway;

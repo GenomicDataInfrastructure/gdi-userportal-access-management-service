a4d6060cd906fd9fe0962587a63e8307a90958f3
Younès Adem <younesadem7@gmail.com>
2025-02-13 10:21:47 +0100

    feat: more api tests

--- src/test/java/io/github/genomicdatainfrastructure/daam/api/InviteMemberTest.java
+++ src/test/java/io/github/genomicdatainfrastructure/daam/api/InviteMemberTest.java
@@ -14,7 +14,7 @@ import static io.restassured.RestAssured.given;
 public class InviteMemberTest extends BaseTest {
 
     @Test
-    void can_invite_member() {
+    void inviteMember() {
         var inviteMember = InviteMember.builder()
                 .name("John")
                 .email("john@example.com")
@@ -29,5 +29,59 @@ public class InviteMemberTest extends BaseTest {
                 .post("/api/v1/applications/12/invite-member")
                 .then()
                 .statusCode(204);
+    }
+
+    @Test
+    void whenApplicationNotFound_inviteMember_returnsNotFound() {
+        var inviteMember = InviteMember.builder()
+                .name("John")
+                .email("john@example.com")
+                .build();
+
+        given()
+                .auth()
+                .oauth2(getAccessToken("alice"))
+                .contentType("application/json")
+                .body(inviteMember)
+                .when()
+                .post("/api/v1/applications/13/invite-member")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void whenUserNotApplicant_inviteMember_returnsForbidden() {
+        var inviteMember = InviteMember.builder()
+                .name("John")
+                .email("john@example.com")
+                .build();
+
+        given()
+                .auth()
+                .oauth2(getAccessToken("alice"))
+                .contentType("application/json")
+                .body(inviteMember)
+                .when()
+                .post("/api/v1/applications/14/invite-member")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    void whenRemsFailure_inviteMember_returnsServerError() {
+        var inviteMember = InviteMember.builder()
+                .name("John")
+                .email("john@example.com")
+                .build();
+
+        given()
+                .auth()
+                .oauth2(getAccessToken("alice"))
+                .contentType("application/json")
+                .body(inviteMember)
+                .when()
+                .post("/api/v1/applications/15/invite-member")
+                .then()
+                .statusCode(500);
     }
 }
